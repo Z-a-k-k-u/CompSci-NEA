19c17f6b70c37aaef289737329b771f5c60606f2
Zach Pemberton <zpembe12@gmail.com>
2021-11-10 07:09:49 +0000

    finally

--- Game/coin/coinmanager.js
+++ Game/coin/coinmanager.js
@@ -2,34 +2,49 @@ function generateCoinPattern(number, platx, platy){
   let coinArray = new Array([])
   let coinx = undefined;
   let coiny = undefined;
+  let currentCoinPattern = undefined; 
   let numberOfCoins = 0;
-  function sameshiz(ychange){
+
+  function coinGenerator(ychange){
     coinx = platx + numberOfCoins
     coiny = platy + ychange
     coinArray.push(new Coin(coinx, coiny))
     numberOfCoins += 20
   }  
+
   switch(number){
     case 1:
-      for(c of CP1){
-        switch(c){
-          case 1:
-            sameshiz(100); 
-            break;
-          case 2:
-            sameshiz(120);
-            break;
-        }
-      }
+      currentCoinPattern = CP1
       break;
     case 2:
-      for(e of CP2) {
-        switch(e) {
-          case 1:
-          sameshiz(100);
-          break; 
-        }
-      }
-    break;
+      currentCoinPattern = CP2
+      break;
+    case 3:
+      currentCoinPattern = CP3
+      break;
+    case 4:
+      currentCoinPattern = CP4
+      break;
+    case 5:
+      currentCoinPattern = CP5
+      break;
+  }
+
+  for(c of currentCoinPattern){
+    switch(c){
+      case 1:
+        coinGenerator(100)
+        console.log(coinArray)
+        break;
+      case 2:
+        coinGenerator(120)
+        console.log(coinArray)
+        break;
+      case 3:
+        coinGenerator(140)
+        console.log(coinArray)
+        break;
+
+    }
   }
 }
